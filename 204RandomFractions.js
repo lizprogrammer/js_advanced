95b858c37711afdbbdf0a250a614babcdfa4415e
Elizabeth <Liz@Jareds-MacBook-Air.local>
2018-03-18 15:55:49 -0400

    Completed assignment

--- 204RandomFractions.js
+++ 204RandomFractions.js
@@ -3,8 +3,11 @@ function randomFraction() {
   // Only change code below this line.
 
     print("Whoa!");
-    return 0;
+    myRandom = Math.random()
+    return myRandom;
 
 
   // Only change code above this line.
 }
+num = randomFraction();
+print(num);
