0dc7a5fd8fe8c118c2ce342ea8c516719a44b57c
Elizabeth <Liz@Jareds-MacBook-Air.local>
2018-03-21 20:42:04 -0400

    Completed 214

---
+++ 214ArrayFunction.js
@@ -0,0 +1,13 @@
+var oldArray = [1,2,3,4,5];
+
+// Only change code below this line.
+var plusThree = oldArray.map(function(val){
+  return val + 3;
+
+});
+
+var newArray = plusThree;
+
+
+print(newArray);
+print(oldArray);
