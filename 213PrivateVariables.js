d3937d80cde982cb55ee7c53418c4a463579bd04
Elizabeth <Liz@Jareds-MacBook-Air.local>
2018-03-21 20:16:03 -0400

    Completed 213

--- 213PrivateVariables.js
+++ 213PrivateVariables.js
@@ -1,24 +1,29 @@
 
-var Car = function() {
+var Car = function()
+{
   // this is a private variable
   var speed = 10;
 
   // these are public methods
-  this.accelerate = function(change) {
+  this.accelerate = function(change)
+  {
     speed += change;
   };
 
-  this.decelerate = function() {
+  this.decelerate = function()
+  {
     speed -= 5;
   };
 
-  this.getSpeed = function() {
+  this.getSpeed = function()
+  {
     return speed;
   };
 
-  this.toString = function(){
-    return "I am a car!  My speed is " + speed + " miles per hour.";
-  }
+  this.toString = function()
+  {
+    return "I am a car!  My speed is " + speed + " mile(s) per hour.";
+  };
 };
 
 var Bike = function() {
@@ -26,21 +31,36 @@ var Bike = function() {
   // Only change code below this line.
   var gear = 5;
 
-  this.getGear(){
+  this.setGear = function(g){
+    gear = g;
+  };
+
+  this.getGear = function(){
     return gear;
-  }
-  this.setGear = function(g)
+  };
+
+  this.toString = function()
   {
-    this.gear = g;
-  }
-    this.toString = function(){
-    return "I am a bike!  I have " + this.gear + "gear(s).";
-  }
+  return "I am a bike!  I have " + gear + " gear(s).";
+  };
+
 };
 
 var myCar = new Car();
 
 var myBike = new Bike();
 
-print(myCar.toString);
-print(myBike.toString);
+myBike.setGear(4);
+
+myBike.getGear();
+
+myBike.setGear(3);
+
+myBike.getGear();
+
+myBike.setGear(1);
+
+myBike.getGear();
+
+print(myCar.toString());
+print(myBike.toString());
