6a96421b68337a83f2481a8a439e5b17fe44a3e7
Elizabeth <Liz@Jareds-MacBook-Air.local>
2018-04-12 19:50:17 -0400

    Adding function.js

---
+++ 257Wherefore.js
@@ -0,0 +1,23 @@
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+
+  for(var words in collection)
+  {
+    if(source.last==collection[words].last)
+        arr.push(collection[words]);
+
+  }
+
+  
+  // Only change code above this line
+  return arr;
+}
+
+var ans = whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
+//print(ans.toSource());
+
+print(ans[0]["a"]);
+print(ans[0]);
---
+++ functions.js
@@ -0,0 +1,38 @@
+function odd (){
+
+	var one = 1;
+	var three = 3;
+	var five = 5;
+
+	return one;
+	/*for (i = 1; i< num; i += 2)
+		print(i);*/
+
+}
+
+
+function even (){
+
+	var two = 2;
+	var four = 4;
+	var six = 6;
+
+	return two;
+
+	/*for (i = 0; i< num; i += 2)
+		print(i);*/
+
+
+}
+
+var ten = 10;
+var eleven = 11;
+
+even();
+odd();
+
+print(odd());
+print(even());
+
+
+
