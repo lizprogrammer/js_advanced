ac76387d089e85453b508a1ebac3d785c7572f60
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-04 23:24:31 -0400

    DNA code

--- 263DNA.js
+++ 263DNA.js
@@ -2,15 +2,41 @@ function pairElement(str) {
 
     var dnaPairs = [];
     var elements = str.split("");
+    var myBuddy = '';
 
-    for(var i = 0; i < elements.length; i++)
-        for(var j = 0; j < elements[i].length; j++)
-            console.log(elements[i]);
+    for(var i = 0; i < elements.length; i++){
+        switch(elements[i]) {
+    case 'A':
+        myBuddy = 'T';
+        break;
+    case 'T':
+        myBuddy = 'A';
+        break;
+    case 'C':
+        myBuddy = 'G';
+        break;
+    case 'G':
+        myBuddy = 'C';
+        break;
+    default:
+        myBuddy = 'B';
+    }
 
-    return str;
+
+        dnaPairs.push([elements[i], myBuddy]);
+
+    }
+
+    return dnaPairs;
 }
 
-pairElement("GCG");
-
+myAns = pairElement("GCG");
+console.log(myAns);
 //[["G", "C"], ["C","G"],["G", "C"]]
+/*
+pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+pairElement("TTGAG") should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+pairElement("CTCTA") should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].*/
+
+
 
